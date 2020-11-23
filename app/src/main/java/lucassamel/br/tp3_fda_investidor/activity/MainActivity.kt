d4042321bf769100fa1bb183c20a483729fe93d8
Lucas Samel <lucas.samel97@gmail.com>
2020-11-23 20:49:35 -0300

    Primeiro fragment_pergunta1.xml Pergunta 1

--- app/src/main/java/lucassamel/br/tp3_fda_investidor/activity/MainActivity.kt
+++ app/src/main/java/lucassamel/br/tp3_fda_investidor/activity/MainActivity.kt
@@ -1,12 +1,28 @@
 package lucassamel.br.tp3_fda_investidor.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import lucassamel.br.tp3_fda_investidor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnIniciar.setOnClickListener {
+
+            //Recebe o valor da variavel da tela
+            var nomeUsuario = edtTxtNome.text.toString()
+            var alterHomeActivit = Intent(
+                this,
+                PerguntasActivity::class.java
+            )
+            //Envia a informacao
+            alterHomeActivit.putExtra("usuarioNome",nomeUsuario)
+
+            startActivity(alterHomeActivit)
+        }
     }
 }
--- app/src/main/java/lucassamel/br/tp3_fda_investidor/activity/PerfilActivity.kt
+++ app/src/main/java/lucassamel/br/tp3_fda_investidor/activity/PerfilActivity.kt
@@ -2,11 +2,18 @@ package lucassamel.br.tp3_fda_investidor.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_perguntas.*
 import lucassamel.br.tp3_fda_investidor.R
 
 class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        var usuarioNome = intent.getStringExtra("usuarioNome")
+
+        var pontuacao = 0
+
+        txtNomeUsuario.text = usuarioNome
     }
 }
